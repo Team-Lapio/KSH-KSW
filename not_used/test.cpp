c9a433384572ef27e0715b9a1e28a680ce12459d
SHGroup <sunghun7511@naver.com>
2018-07-13 01:07:01 +0900

    change structure

--- not_used/test.cpp
+++ not_used/test.cpp
@@ -1,14 +1,12 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
 #define UNW_LOCAL_ONLY
-#include <libunwind.h> 
+#include <libunwind.h>
 
-
- 
 static void print_backtrace(void)
 {
     unw_context_t context;
@@ -17,51 +15,50 @@ static void print_backtrace(void)
     unw_proc_info_t pip;
     char procname[256];
     int ret;
-    int a =0;
+    int a = 0;
     a = 3;
- //
 
     if (unw_getcontext(&context))
         return;
- 
+
     if (unw_init_local(&cursor, &context))
         return;
- 
+
     while (unw_step(&cursor) > 0) {
         if (unw_get_proc_info(&cursor, &pip))
             break;
- 
+
         ret = unw_get_proc_name(&cursor, procname, 256, &off);
         if (ret && ret != -UNW_ENOMEM) {
             procname[0] = '?';
             procname[1] = 0;
         }
- 
+
         unw_get_reg(&cursor, UNW_REG_SP, &sp);
- 
-        fprintf(stderr,"sp = 0x%lx\n",(long)sp);
-    
+
+        fprintf(stderr, "sp = 0x%lx\n", (long)sp);
+
         //fprintf(stderr, "ip = 0x%lx (%s), sp = 0x%lx\n", (long)ip, procname, (long)sp);
     }
 
     printf(" \n");
     // fprintf(stderr, "NEW:ip = 0x%lx (%s), sp = 0x%lx\n\n", (long)ip, procname, (long)sp);
 }
- 
+
 void dummy(void)
 {
     print_backtrace();
 }
- 
+
 int main(void)
 {
     dummy();
     printf("----------------------------\n");
-    struct Node{
-        int data=0;
+    struct Node {
+        int data = 0;
     };
     Node x;
-    int a =3;
+    int a = 3;
     x.data = 10;
     dummy();
     return 0;
